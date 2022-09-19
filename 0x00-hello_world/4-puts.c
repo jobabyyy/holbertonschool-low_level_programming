122157855acc15ffaaf8a8759b6151e0f941c2ad
jobabyyy <joeannaavila123@gmail.com>
2022-09-19 09:27:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'programming is multilingual'
+ *
+ * Return: Always 0 
+ */
 int main(void)
 { 
 	puts("\"Programming is like building a multilingual puzzle");	

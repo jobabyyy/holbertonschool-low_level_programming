a2158a01f16b37ab59f5590a532aadb4b25ebf8b
jobabyyy <joeannaavila123@gmail.com>
2022-09-20 15:15:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#incldue <stdio.h>
+#incldue <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int hi;
+	for (hi = '0'; hi <= '9'; hi++)
+		putchar(hi);
+	putchar('\n');
+	return (0);
+
+}

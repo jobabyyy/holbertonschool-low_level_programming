2c4ff4aa24e5166ba8c1b3a4a142a5cc5bbbb7bf
jobabyyy <joeannaavila123@gmail.com>
2022-09-20 16:25:03 -0700

    4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; b++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
+

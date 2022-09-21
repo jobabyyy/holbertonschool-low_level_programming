4f3ab8750e399d64eab2352d2ba645f80205dcf6
jobabyyy <joeannaavila123@gmail.com>
2022-09-20 17:24:25 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,15 @@
  */
 int main(void)
 {
-	char a = "_putchar";
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	putchar(a);
-	return (0);
+return (0);
 }

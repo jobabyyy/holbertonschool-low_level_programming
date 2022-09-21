ae9befb4b33a2b22b5507a5b5b9bf38d96a746c5
jobabyyy <joeannaavila123@gmail.com>
2022-09-20 18:41:51 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,14 +14,14 @@ int last_digit;
 
 if (n < 0)
 {
-last digit = (-1 * (n % 10));
+last_digit = (-1 * (n % 10));
 _putchar (last_digit + '0');
 return (last_digit);
 	}
-}
 
 else
-{
+:x
+
 last_digit = (n % 10);
 _putchar (last_digit + '0');
 return (last_digit);

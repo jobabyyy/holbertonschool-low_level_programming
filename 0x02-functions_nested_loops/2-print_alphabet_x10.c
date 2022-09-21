905e5972752c664c0b69d1ed5f2dcfee9e523fe4
jobabyyy <joeannaavila123@gmail.com>
2022-09-20 18:01:18 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,20 +2,21 @@
 /**
  * print_alphabet_x10 - use _putchar to print in
  * lowercase 10xs
+ *
  * Return: Always 0 (Success)
  *
  */
 void print_alphabet_x10(void)
 {
-	int o
-	int l;
+int o
+int l;
 
-	l = 0;
-	while (l < 10)
-	{
-	for (o = 'a' ; o <= 'z'; o++)
-	}
-	_putchar(o);
+l = 0;
+while (l < 10)
+{
+for (o = 'a' ; o <= 'z'; o++)
+}
+_putchar(o);
 }
 
 l++;

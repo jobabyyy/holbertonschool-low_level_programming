052050f4c528483175e2f5f7182ba64a0a020548
jobabyyy <joeannaavila123@gmail.com>
2022-09-20 18:39:26 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,21 @@
 int print_last_digit(int n)
 
 {
-	int last_digit;
+int last_digit;
 
-	if (n < 0)
-	{
-		last digit = (-1 * (n % 10));
-		_putchar (last_digit + '0');
-		return (last_digit)
+if (n < 0)
+{
+last digit = (-1 * (n % 10));
+_putchar (last_digit + '0');
+return (last_digit);
 	}
+}
+
+else
+{
+last_digit = (n % 10);
+_putchar (last_digit + '0');
+return (last_digit);
+}
+
 }

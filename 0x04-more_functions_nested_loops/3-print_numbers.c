e93a1ce0ccec6f76a47b39131631ebb17a604873
jobabyyy <joeannaavila123@gmail.com>
2022-09-23 14:37:50 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,20 +5,13 @@
  *
  * Return: numbers and new line
  */
-int print_numbers(void)
+void print_numbers(void)
 {
 	int i;
 	
 	for (i = 0; i <= 9; i++)
 	{
-		if (i == 2 && i == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
+		_putchar(i);
 	}
+		_putchar('\n')
 }

ac7d0345c221c4638fa92eb7df2912c364b8c4b7
jobabyyy <joeannaavila123@gmail.com>
2022-09-23 08:37:28 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - returns 1 and c if uppercase
+ *
+ * @c: Variable
+ *
+ * Return: 1
+ */
+int _isupper(int c)
+
+{
+	for (c >= 65 && c <= 90)
+		
+		return 1
+}
+
+	else {
+	
+		return 0
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Nested while loops, C funtions, header files and 
+how to use a prototype function.

2b6947b891f1cd753b082d896cb36d6d1e44d6b1
jobabyyy <joeannaavila123@gmail.com>
2022-09-25 13:51:02 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
- * 4-print_most_numbers - print nums 0-9 skip 2,4
+ * print_most_numbers - print nums 0-9 skip 2,4
  *
- *
+ * Return: skip 4 & 2
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a == '2' && a == '4')
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(a);
+		}
+			_putchar('\n');
+	}
+}

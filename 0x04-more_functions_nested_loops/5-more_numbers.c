8379cc8b0ccb389733582200a81fa4437d3e40a2
jobabyyy <joeannaavila123@gmail.com>
2022-09-26 14:03:32 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,4 +6,16 @@
  */
 void more_numbers(void)
 {
-	int 
+	int x, i;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + 48);
+			_putchar(i % 10 + 48);
+		}
+		_putchar('\n');
+	}
+}

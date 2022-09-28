b810b634356ed8605723de5a88cd95daef6919f0
jobabyyy <joeannaavila123@gmail.com>
2022-09-28 08:59:35 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: variable
+ *
+ * Return: Value
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while(i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+	

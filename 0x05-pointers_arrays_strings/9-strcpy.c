4d27c99b955af7264bc098bdf1c388c34f364834
jobabyyy <joeannaavila123@gmail.com>
2022-09-28 12:37:08 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strcpy - copy string pointed to by src
+ * *_strcpy - copy string pointed to by src
  * @dest: var1
  * @scr: var2
  * Return: 0
  */
-char *_strcpy(char *dest char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int b = 0;
 
@@ -14,6 +14,7 @@ char *_strcpy(char *dest char *src)
 		dest[b] = src[b];
 		b++;
 	}
+	dest[b] = '\0';
 	return (dest);
 }
 

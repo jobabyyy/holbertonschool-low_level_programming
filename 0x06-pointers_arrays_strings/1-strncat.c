8ea8c8406680f471ea89d3fa6316e596be0c0dcb
jobabyyy <joeannaavila123@gmail.com>
2022-09-30 15:07:42 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *_strncat - concatenate 2 strings but do
+ * not include null.
+ *
+ * @dest: char1
+ * @src: char2
+ * @n: char3
+ *
+ * Return: 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen = 0;
+      	int i;
+
+	while (dest[dlen] != '\0')
+		++dlen;
+	for ( i = 0; i < n && src[i] != '\0'; i++)
+		dest[dlen + i] = '\0';
+	return (dest);
+}	

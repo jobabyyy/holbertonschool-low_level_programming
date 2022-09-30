a533865744bfd65c7abc39172f1e7585e638425a
jobabyyy <joeannaavila123@gmail.com>
2022-09-30 15:52:59 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,8 +15,13 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 
 	while (dest[dlen] != '\0')
-		++dlen;
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dlen + i] = '\0';
+	{
+		dlen++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++, dlen++)
+	{
+		dest[dlen] = src[i];
+	}
+	dest[dlen] = '\0';
 	return (dest);
 }

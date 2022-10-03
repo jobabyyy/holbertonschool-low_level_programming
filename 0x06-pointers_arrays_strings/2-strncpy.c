df92402bd69691ed1292e0734fd4fa0f2f7c49ac
jobabyyy <joeannaavila123@gmail.com>
2022-10-03 16:04:25 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len;
+	int i;
 
-	while (dest[len] != '\0')
-
-	{
-		for (len = 0; len < n && src[len] != '\0'; len++)
-			dest[len] = src[len];
-	       	for ( ; len < n; len++)
-			dest[len] = '\0';
-	}
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+			dest[i] = src[i];
+	for ( ; i < n; i++)
+			dest[i] = '\0';
 	return dest;
 }

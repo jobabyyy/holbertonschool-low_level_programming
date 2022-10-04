269bdb013873929b3e4afd7802ca1efe225c3bb2
jobabyyy <joeannaavila123@gmail.com>
2022-10-04 15:48:28 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - write a function that fills a memoru with a constant byte.
+ *
+ * @s: char1
+ * @b: char2
+ * @n: char3
+ * 
+ * Return: 0.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+		
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+How to use pointers to pointers, what are multidimensional arrays and how to use them
+as well as the most common C standard library functions to manipulate strings.

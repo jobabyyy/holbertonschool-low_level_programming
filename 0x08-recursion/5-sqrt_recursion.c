9120de9cb5a056e5907e5aaf7a30be4a2747cbfb
jobabyyy <joeannaavila123@gmail.com>
2022-10-11 16:31:07 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@
  *
  * Return 0.
  */
-int helper_sqrt(int n, int i);
+int helper_sqrt(int n, int i)
 
 int _sqrt_recursion(int n)
 {
@@ -32,5 +32,5 @@ int helper_sqrt(int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (helper_sqrt(n i + 1));
+	return (helper_sqrt(n, i + 1));
 }

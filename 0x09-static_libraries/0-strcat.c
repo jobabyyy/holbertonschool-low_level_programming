f137c917d1aa3a1f19b5df75f343b30eb936635e
jobabyyy <joeannaavila123@gmail.com>
2022-10-11 14:23:07 -0700

    main.h

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenate two strings.
+ *
+ * @dest: character1
+ * @src: character2
+ *
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i]; i++)
+
+		;
+
+		for (j = 0; src[j]; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
+		return (dest);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - print string followed by
+ * newline to stdout.
+ *
+ * @str: int
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

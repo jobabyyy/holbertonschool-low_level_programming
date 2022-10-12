e857aac19f0348ec626086429693890ccf860267
jobabyyy <joeannaavila123@gmail.com>
2022-10-11 20:05:49 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 /**
  * main - program that prints
- * all arguments it receives.
+ * all arguments passed onto it..
  *
- * @args: counter
+ * @argc: counter
  * @argv: arguement.
  *
  * Return: 0.
  */
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
-    if (argv[0])
-	    printf("%d\n", argc -1);
-    return (0);
+	if (argv[0])
+		  printf("%d\n", argc - 1);
+      	return (0);
 }

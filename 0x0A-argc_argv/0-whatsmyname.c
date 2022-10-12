03348c2044372a8d8fffce7482b658346c502c3f
jobabyyy <joeannaavila123@gmail.com>
2022-10-11 18:26:58 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - program that prints num of arguements passed.
+ *
+ * @argc: arg
+ * @argv: argument passed.
+ *
+ * Return: 0.
+ */
 int main(int argc, char **argv[])
 {
 	printf("mynameis\n");

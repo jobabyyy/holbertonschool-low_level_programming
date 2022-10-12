9804c02d851858a3648056c3c5106c320966644c
jobabyyy <joeannaavila123@gmail.com>
2022-10-12 15:38:32 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 
 	s = (char *) malloc(size * sizeof(char));
 	if (s == NULL)
-		return ('\0');
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		s[i] = c;

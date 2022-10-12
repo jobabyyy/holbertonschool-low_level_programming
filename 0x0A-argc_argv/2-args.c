69ea853f9fff1adae8983223217c8dc27c5de791
jobabyyy <joeannaavila123@gmail.com>
2022-10-11 19:20:56 -0700

    README.md

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,20 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * main - program that prints
+ * all arguments it receives.
+ *
+ * @args: counter
+ * @argv: arguement.
+ *
+ * Return: 0.
+ */
 int main(int args, char *argv[])
 {
 	int i = 0;
-	
+
 	for (i = 0; i < args; i++)
 		printf("\n%s", argv[i]);
 
-	return 0;
+	return (0);
 }
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,7 @@
+-Arguments to main
+-argc and argv
+-What does argc and argv mean?
+-how to compile with unused variablesArguments to main.
+
+// learning how to use argc and argv and to compile using attribute 
+((unused)) or (void)//

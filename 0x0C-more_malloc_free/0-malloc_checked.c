2c28f6de34128bad56f35be5d7110c4271cff223
jobabyyy <joeannaavila123@gmail.com>
2022-10-18 14:41:07 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc.
+ *
+ * @b: unsigned integer
+ *
+ * Return: 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}

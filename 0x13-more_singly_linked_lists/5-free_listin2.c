a7319ad6e960cfae3a07a2d64eacb605e1001460
jobabyyy <joeannaavila123@gmail.com>
2022-11-08 15:11:33 -0800

    5-free_listin2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listin2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listin2 - free list
+ * @head: starting header
+ *
+ * Return: NULL or HEAD.
+ */
+void free_listint2(listint_t **head);
+{
+	listint_t *temp;
+
+	if (!head)
+	{
+		return;
+	}
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head)
+		*head = temp;
+	}
+	head = NULL;
+}

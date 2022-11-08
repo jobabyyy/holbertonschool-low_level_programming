6608532ac57cfb7811dee4d8a76378f4c5e0fc01
jobabyyy <joeannaavila123@gmail.com>
2022-11-08 11:35:56 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,22 +8,22 @@
 #include <stdarg.h>
 
 /**
- * struct listint_s - singly linked list
+ * struct sll - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  *
  */
-typedef struct listint_s
+typedef struct sll
 {
 	int n;
-	struct listint_s *next;
+	struct sll *next;
 } listint_t;
 
 /** prototypes*/
 
-print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h);
 
 size_t listint_len(const listint_t *h);
 
@@ -41,7 +41,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 
 int sum_listint(listint_t *head);
 
-listint_t *insert_nodeint_at_index(listint_t **head unsigned int idx, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 

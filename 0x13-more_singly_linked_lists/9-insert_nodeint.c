c032b13211641b576edc278469440ec14e797953
jobabyyy <joeannaavila123@gmail.com>
2022-11-09 09:16:39 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,4 +12,32 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *nNode, *temp = *head;
+	unsigned int node;
 
+	nNode = malloc(sizeof(listint_t));
+	if (nNode == NULL)
+		return (NULL);
+
+	nNode->n = n;
+
+	if (idx == 0)
+	{
+		nNode->next = temp;
+		*head = nNode;
+		return (nNode);
+	}
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (temp == NULL || temp->next == NULL)
+			return (NULL);
+
+		temp = temp->next;
+	}
+	
+	nNode->next = temp->next;
+	temp->next = nNode;
+
+	return (nNode);
+}

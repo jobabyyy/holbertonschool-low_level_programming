a7680100eb4c25098c59990d4e78a4de2b7f4c2a
jobabyyy <joeannaavila123@gmail.com>
2022-11-09 17:01:04 -0800

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/** 
+/**
  * clear_bit - function that sets value of a bit to a 0 at given index.
  *
- * #n: bit value
+ * @n: bit value
  * @index: given index
  *
  * Return: 1 if success, -1 if fail

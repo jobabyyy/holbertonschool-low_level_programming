725c173adb84bb73d362d48111e24d2c52637633
jobabyyy <joeannaavila123@gmail.com>
2022-11-14 14:54:53 -0800

    9-print_dlistint_backward.c

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_dlistint_backward - function that prints
+ * all elements of a list backwards.
+ * @h: head pointer
+ *
+ * Return: # of nodes.
+ */
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+	size_t nodeCounter = 0, newNode;
+
+	if (!(h))
+	{
+		return (nodeCounter);
+	}
+
+	while (h->next)
+	{
+		h = h->next;
+		nodeCounter++;
+	}
+
+	newNode = nodeCounter;
+	for (; nodeCounter > 0; newNode--)
+	{
+		printf("%d\n", h->n);
+		h = h->prev;
+	}
+	printf("%d\n", h->n);
+	newNode++;
+	return (newNode);
+}

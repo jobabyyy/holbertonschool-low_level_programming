8877af65766eb2bf2997445ef5ddf495473892ae
jobabyyy <joeannaavila123@gmail.com>
2022-11-15 14:44:25 -0800

    hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -41,7 +41,7 @@ typedef struct hash_table_s
 
 /** functions and prototypes */
 
-hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size);
 
 unsigned long int hash_djb2(const unsigned char *str);
 

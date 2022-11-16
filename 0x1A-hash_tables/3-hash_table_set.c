d793b835a1e35577bafcc66f562610d931d1c0e7
jobabyyy <joeannaavila123@gmail.com>
2022-11-16 11:16:34 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,8 +38,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (0);
 	}
 	newHash->key = strdup(key);
-	if (newHash == NULL)
-	{
+	if (newHash->key == NULL)
+	
 		free(newHash);
 		return (0);
 	}

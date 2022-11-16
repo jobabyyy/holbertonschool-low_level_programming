ab3f6d483cd5a18b9856e2f65da06c6451188ce0
jobabyyy <joeannaavila123@gmail.com>
2022-11-16 09:42:51 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,12 +11,40 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_table_t *NewHash;
+	unsigned long int i;
+	unsigned long int index;
+	char *value_copy;
+	hash_node_t *newHash;
 
-	NewHash = (hash_table_t *)malloc(sizeof(hash_table_t));
-	NewHash->key = key;
-	NewHash->prox = NULL;
 
-	NewHash->prox = ht->ListKey[value];
-	ht->ListKey[value] = NewHash;
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	return (0);
+
+	value_copy = strdup(value);
+	if(value_copy == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp (ht-> array[i]->key, key) == 0)
+		{
+			free(ht-> array[i]->value);
+			ht->array[i]->value = value_copy;
+		}
+	}
+	newHash = malloc(sizeof(hash_node_t));
+	{
+		if (newHash == NULL)
+			return (0);
+	}
+	newHash->key = strdup(key);
+	if (newHash == NULL)
+	{
+		free(newHash);
+		return (0);
+	}
+	newHash->value = value_copy;
+	newHash->next = ht->array[index];
+	ht->array[index] = newHash;
+	return (1);
 }

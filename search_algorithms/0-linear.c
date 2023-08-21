c2b7cf03a1f2bc440397fe756989fdba130f6784
Johanna Avila <joeannaavila123@gmail.com>
2023-08-21 16:41:10 -0500

    testing betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,7 +3,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in an array using Linear search algorithm
+ * linear_search - Searches for value in an array using Linear search algorithm
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in the array
  * @value: The value to search for
@@ -13,8 +13,8 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
-    
+   size_t i;
+
     if (array == NULL)
         return -1;
 
